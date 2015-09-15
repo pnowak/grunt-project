f891af474cfafef030cad600f3d30b2f28b03dc6
pnowak <piotr.nowak.25.02.1978@gmail.com>
2015-09-15 23:29:31 +0200

    compile less files to css files, minified and watch changes

--- grunt-project/Gruntfile.js
+++ grunt-project/Gruntfile.js
@@ -1,5 +1,32 @@
 module.exports = function(grunt) {
-	grunt.registerTask('default', function() {
-		grunt.log.writeIn('Hello from Grunt.');
+	grunt.initConfig({
+		less: {
+			styles: {
+				src: 'src/less/style.less',
+				dest: 'dist/css/style.css'
+			}
+		},
+		cssmin: {
+			options: {
+	            keepSpecialComments: 0
+	        },
+	        my_target: {
+	            src: 'dist/css/style.css',
+	            dest: 'dist/css/style.min.css'
+	        }
+	    },
+	    watch: {
+	    	options: {
+		        livereload: true,
+		    },
+		    css: {
+		        files: ['src/less/style.less'],
+		        tasks: ['less'],
+		    }
+	    }
 	});
+	grunt.loadNpmTasks('grunt-contrib-less');
+	grunt.loadNpmTasks('grunt-contrib-cssmin');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.registerTask('default', ['less', 'cssmin', 'watch']);
 }
